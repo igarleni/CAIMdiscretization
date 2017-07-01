31ef95360315a71e31af4c42f1e5c221ec5ee6c3
italogar6 <italogar6@hotmail.com>
2017-07-01 22:48:52 +0100

    Reading input args

---
+++ src/Constants.scala
@@ -0,0 +1,43 @@
+
+
+object Constants {
+  var FILE_INPUT: String = null
+  var FILE_CP_OUTPUT: String = null
+  var FILE_RUNTIME_OUTPUT: String = null
+  var FILE_DATA_OUTPUT: String = null
+  var FILE_ORG_INPUT: String = null
+  var FILE_ORG_NORMAL_INPUT: String = null
+  var FILE_IDIST: String = null
+  
+  var NUM_ROWS = 0
+  var NUM_MEASURE_COLS = 0
+  var NUM_CAT_CONTEXT_COLS = 0
+  var NUM_ORG_CAT_CONTEXT_COLS = 0
+  
+  var FIELD_DELIMITER = ";"
+  
+  var MAX_VAL:Double = 0.0
+  
+  var METHOD:String = null
+  val MB_MDL_DP = 0	  // MDL dynamic programming
+	val MB_MDL_GD = 1  	// MDL greedy
+	val MB_MDL_EGD = 2	// normal greedy
+	val EW = 3		  	  // equal-width
+	val EF = 4    			// equal-frequency
+	val CAIM = 5	    	// equal-frequency
+	val MB_NM_GD = 6  	// normal greedy
+	val J_MDL = 9    		// normal greedy
+	val J_MDL_HM = 10  	// normal greedy
+	val J_GD_HM = 11  	// normal greedy
+	val M_GD_CAIM = 7  	// normal greedy
+	val M_GD = 8		    // normal greedy
+	val MMIC = 12		    // normal greedy
+	val FayyadMDL = 14	// normal greedy
+	val KoMDL = 15	  	// normal greedy
+	val DP_MEAN = 16  	// MDL dynamic programming
+	val DP_MIN = 17    	// MDL dynamic programming
+	val DP_MAX = 18	    // MDL dynamic programming
+	val MVD = 19	      // MDL dynamic programming
+	val PCABINNING = 20
+	
+}
--- src/MainClass.scala
+++ src/MainClass.scala
@@ -4,7 +4,8 @@ import org.apache.spark.rdd._
 import org.apache.spark.SparkConf
 
 object MainClass {
-  def main(args:Array[String]): Unit = {
+  def main(args:Array[String]): Unit = 
+  {
     val conf = new SparkConf()
     conf.set("spark.cores.max", "20")
     conf.set("spark.executor.memory", "6g")
@@ -12,7 +13,152 @@ object MainClass {
     conf.setAppName("IPDdiscretization")
     val sc = new SparkContext(conf)
     
-    println("Hello World")
+    println("----------------App init----------------")
+    try
+    {
+      //readInputString(args)
+      println("----------------App end----------------")
+    }
+    catch
+    {
+      case ex:Exception => ex.printStackTrace
+    }
+  }
+  
+  def readInputString(args:Array[String]): Unit = 
+  {
+    val total = args.length -1
     
+    var found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FILE_INPUT"))
+      {
+        Constants.FILE_INPUT = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FILE_INPUT");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FILE_IDIST"))
+      {
+        Constants.FILE_IDIST = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FILE_IDIST");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FILE_CP_OUTPUT"))
+      {
+        Constants.FILE_CP_OUTPUT = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FILE_CP_OUTPUT");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FILE_RUNTIME_OUTPUT"))
+      {
+        Constants.FILE_RUNTIME_OUTPUT = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FILE_RUNTIME_OUTPUT");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FILE_DATA_OUTPUT"))
+      {
+        Constants.FILE_DATA_OUTPUT = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FILE_DATA_OUTPUT");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-NUM_ROWS"))
+      {
+        Constants.NUM_ROWS = args(i+1).toInt
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -NUM_ROWS");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-NUM_MEASURE_COLS"))
+      {
+        Constants.NUM_MEASURE_COLS = args(i+1).toInt
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -NUM_MEASURE_COLS");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-NUM_CAT_CONTEXT_COLS"))
+      {
+        Constants.NUM_CAT_CONTEXT_COLS = args(i+1).toInt
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -NUM_CAT_CONTEXT_COLS");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-FIELD_DELIMITER"))
+      {
+        Constants.FIELD_DELIMITER = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -FIELD_DELIMITER");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-MAX_VAL"))
+      {
+        Constants.MAX_VAL = args(i+1).toInt
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -MAX_VAL");
+    
+    found = false
+    for (i <- 0 until total if !found)
+    {
+      if (args(i).equals("-METHOD"))
+      {
+        Constants.METHOD = args(i+1)
+        found = true
+      }
+    }
+    if (found == false)
+			throw new Exception("Missing -METHOD");
   }
 }
